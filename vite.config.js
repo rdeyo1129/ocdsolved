5e4ad5e66fa20627118afdebe20232491571f563
Ryan Deyo <rdeyo1129@gmail.com>
2023-04-08 17:55:56 -0400

    add favicon

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,10 @@ import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    assetsDir: "public/assets",
+    rollupOptions: {
+      input: "src/main.js",
+    },
+  },
 });
